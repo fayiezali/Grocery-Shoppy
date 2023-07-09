dcc1d5d943cf567cd02eb5cfa8f6c7164ce0c383
fayiezali <fayiez@outlook.com>
2023-07-09 14:56:11 +0300

    Add Login

--- src/login/urls.py
+++ src/login/urls.py
@@ -5,6 +5,6 @@ from . import views # This Views I Created It
 # # AUTHENTICATION:-------------------------------------------------------------------------------------------------------
 urlpatterns = [
         # Display Login Web Page
-        path('login_pag/' , views.login_page_DEF   ,  name='login_page-URL' ),
-        path("login/"     , views.Login            , name="login"),
+        # path('login_pag/' , views.login_page_DEF   ,  name='login_page-URL' ),
+        path("login/"     , views.Login            , name="login-URL"),
 ] 
--- src/login/views.py
+++ src/login/views.py
@@ -1,12 +1,12 @@
 from django.shortcuts import render , redirect
 from django.contrib import messages
-from django.contrib.auth import login, authenticate, logout 
+from django.contrib.auth import login, authenticate 
 
 
 
 # Display Login Web Page
-def login_page_DEF(request):
-    return render(request,'login_logout/login.html', {})
+# def login_page_DEF(request):
+#     return render(request,'login_logout/login.html', {})
 
 
 def Login(request):
@@ -16,12 +16,25 @@ def Login(request):
         if request.method == "POST":
             username = request.POST['username']
             password = request.POST['password']
+            remember = request.POST.get('remember_me')
             user = authenticate(username=username, password=password)
 
             if user is not None:
+                if remember is None:
+                    request.session.set_expiry(0)  # <-- Here if the remember me is False, that is why expiry is set to 0 seconds. So it will automatically close the session after the browser is closed.
+                    messages.info(request    , "Session Expiry.")
                 login(request, user)
+                messages.success(request, f'Welcome: ( {username} )')
                 return redirect("/")
+                # if 'remember_me' not in request.POST:
+                #     request.session.set_expiry(0)  # <-- Here if the remember me is False, that is why expiry is set to 0 seconds. So it will automatically close the session after the browser is closed.
+                #     messages.info(request    , "Session Expiry.")
+                # login(request, user)
+                # messages.success(request, f'Welcome: ( {username} )')
+                # return redirect("/")
             else:
-                alert = True
-                return render(request, "login/login.html", {"alert":alert})
+                messages.error(request   , "User Name or Password Is Incorrect.") 
+                return render(request, "login/login.html")
+                # alert = True
+                # return render(request, "login/login.html", {"alert":alert})
     return render(request, "login/login.html")
